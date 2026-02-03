ee9a0b5d8ab3e412587748a2af5163245c9750a5
Brian Bennett <bennettb15@gmail.com>
2026-02-03 09:44:17 -0500

    Initial Commit

---
+++ ScoutCapture/ScoutCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutCaptureApp.swift
+//  ScoutCapture
+//
+//  Created by Brian Bennett on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoutCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
